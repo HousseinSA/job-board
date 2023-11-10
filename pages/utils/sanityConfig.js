bffed73127b8d6d6b061a22fdb42278ce762953e
Hussein Sedoum <96076240+HousseinSA@users.noreply.github.com>
2023-11-10 17:01:40 +0000

    Delete pages/utils directory

--- pages/utils/sanityConfig.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from "next-sanity"
-
-export const client = createClient({
-  projectId: "b0kwbpge",
-  dataset: "production",
-  apiVersion: "v2021-10-21",
-  useCdn: false,
-})
